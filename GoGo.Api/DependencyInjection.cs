ff1903f7586375a4368088f4692b522b91753dac
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2025-08-06 14:34:49 +0700

    Add project files.

---
+++ GoGo.Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using GoGo.Application;
+using GoGo.Infrastructure;
+
+namespace GoGo.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
---
+++ GoGo.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoGo.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection service)
+        {
+            return service;
+        }
+
+    }
+}
